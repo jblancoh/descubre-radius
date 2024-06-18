32d6a8ee5fe51f1cc022758e4f1ab87b91cc5bf6
Ubuntu <zlatanjon@hotmail.com>
2024-06-18 20:38:16 +0000

    refactor pm2

--- cms-radius/ecosystem.config.js
+++ cms-radius/ecosystem.config.js
@@ -2,24 +2,24 @@ module.exports = {
     apps: [
       {
       name: 'Descubre radius',
-      cwd: '/home/ubuntu/descubre-radius',
+      cwd: '/home/ubuntu/descubre-radius/cms-radius',
       script: 'npm',
       args: 'start',
       env: {
-        APP_KEYS: process.env.APP_KEYS,
-        API_TOKEN_SALT: process.env.API_TOKEN_SALT,
-        ADMIN_JWT_SECRET: process.env.ADMIN_JWT_SECRET,
-        JWT_SECRET: process.env.JWT_SECRET,
+        APP_KEYS:"XxtyjVPS9qpVaITJ4IX1ag==,b2hisyehgu6+Mqx31NCXtw==,s1wE5LywR9ZdGTBfLhyFBw==,OW2jXF9liF2pFLSxI4uKGw==",
+        API_TOKEN_SALT: "+nzbDy8KEYUw3rMlqk3vZw==",
+        ADMIN_JWT_SECRET: "dCwIyPrQAtkBwfUp90BFMA==",
+        JWT_SECRET: "h7WamnnL9uIuKPdGN4TsDA==",
         NODE_ENV: 'production',
-        DATABASE_HOST: process.env.DATABASE_HOST,
+        DATABASE_HOST: "radius-strapi.cheyh15e6e8t.us-east-2.rds.amazonaws.com",
         DATABASE_PORT: '5432',
-        DATABASE_NAME: process.env.DATABASE_NAME,
-        DATABASE_USERNAME: process.env.DDATABASE_USERNAME,
-        DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
-        AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-        AWS_ACCESS_SECRET: process.env.AWS_ACCESS_SECRET,
-        AWS_REGION: process.env.AWS_REGION,
-        AWS_BUCKET_NAME: process.env.AWS_BUCKET_NAME
+        DATABASE_NAME: "postgres",
+        DATABASE_USERNAME: "postgres",
+        DATABASE_PASSWORD: "gLmNQ9HZ",
+        AWS_ACCESS_KEY_ID: "AKIAUBWUXHEHHK5ZW5XR",
+        AWS_ACCESS_SECRET: "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",
+        AWS_REGION: "us-east-2",
+        AWS_BUCKET_NAME: "strapiradiusbucket"
       },
     }
   ],
