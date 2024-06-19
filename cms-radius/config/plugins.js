d3b0de76224059c57a74c2b73aed05a3f1725db5
jonathan <zlatanjon@hotmail.com>
2024-06-18 23:00:22 -0600

    chore: Update AWS S3 credentials in plugins.js

--- cms-radius/config/plugins.js
+++ cms-radius/config/plugins.js
@@ -7,9 +7,11 @@ module.exports = ({env}) => ({
       provider: 'aws-s3',
       providerOptions: {
         s3Options: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
+          credentials: {
+            accessKeyId: env('AWS_ACCESS_KEY_ID'),
+            secretAccessKey: env('AWS_ACCESS_SECRET'),
+            region: env('AWS_REGION'),
+          },
           params: {
             Bucket: env('AWS_BUCKET_NAME'),
           },
