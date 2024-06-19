5a2b744242078deeed16a521684a547efb5b6398
jonathan <zlatanjon@hotmail.com>
2024-06-19 00:07:58 -0600

    chore: Update admin.js, middlewares.js, and server.js configurations

--- cms-radius/config/middlewares.js
+++ cms-radius/config/middlewares.js
@@ -2,6 +2,19 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'default-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://strapiradiusbucket.s3.us-east-2.amazonaws.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https://strapiradiusbucket.s3.us-east-2.amazonaws.com'],
+          'connect-src': ["'self'", 'https:'],
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
