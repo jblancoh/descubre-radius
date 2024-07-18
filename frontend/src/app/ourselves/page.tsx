5a0ed8e4355c2d8468507b174b30992bd6cadafe
JamesGH1 <jaime.santisteban@radiustech.mx>
2024-07-18 15:47:04 -0600

    (Advancements): Creation of directory path for "Nosotros (Ourselves)", creation of first banner and "Radius Hoy"

---
+++ frontend/src/app/ourselves/page.tsx
@@ -0,0 +1,13 @@
+import OurselvesBanner from "@/components/OurselvesBanner";
+import RadiusToday from "@/components/RadiusToday";
+
+const Page = () => {
+  return (
+    <div>
+      <OurselvesBanner />
+      <RadiusToday />
+    </div>
+  )
+}
+
+export default Page;
