2fc45756c3f357d80d7f7ff509fc1c9aa85c253d
MikeGsus <mike08ahj@hotmail.com>
2024-07-25 16:02:28 -0600

    feat(blog): add disabled options to pagination buttons

--- frontend/src/components/ui/pagination.tsx
+++ frontend/src/components/ui/pagination.tsx
@@ -36,26 +36,30 @@ PaginationItem.displayName = "PaginationItem"
 
 type PaginationLinkProps = {
   isActive?: boolean
-} & Pick<ButtonProps, "size"> &
+} & Pick<ButtonProps, "size"> & 
+  Pick<ButtonProps, "disabled"> &
   React.ComponentProps<"a">
 
 const PaginationLink = ({
   className,
   isActive,
   size = "icon",
+  disabled = false,
   ...props
 }: PaginationLinkProps) => (
-  <a
-    aria-current={isActive ? "page" : undefined}
-    className={cn(
-      buttonVariants({
-        variant: isActive ? "outline" : "ghost",
-        size,
-      }),
-      className
-    )}
-    {...props}
-  />
+  <button disabled={disabled} >
+    <a
+      aria-current={isActive ? "page" : undefined}
+      className={cn(
+        buttonVariants({
+          variant: disabled ? "disabled" : isActive ? "outline" : "ghost",
+          size,
+        }),
+        className
+      )}
+      {...props}
+    />
+  </button>
 )
 PaginationLink.displayName = "PaginationLink"
 
