79fb86b103819a36065847d6e65d74f2e7d23ceb
MikeGsus <mike08ahj@hotmail.com>
2024-07-29 15:46:40 -0600

    feat(worker): remove custom slug generation on worker update

--- cms-radius/src/api/worker/content-types/worker/lifecycles.js
+++ cms-radius/src/api/worker/content-types/worker/lifecycles.js
@@ -1,12 +1,9 @@
 const slugify = require('slugify');
 
 module.exports = {
+    // custom slug generation only happens on create
+    // this is because we want to allow the user to change it later
     async beforeCreate(event) {
-      if (event.params.data.name && event.params.data.lastName) {
-        event.params.data.slug = slugify(`${event.params.data.name} ${event.params.data.lastName}`, {lower: true});
-      }
-    },
-    async beforeUpdate(event) {
       if (event.params.data.name && event.params.data.lastName) {
         event.params.data.slug = slugify(`${event.params.data.name} ${event.params.data.lastName}`, {lower: true});
       }
