c9e8fcde78635b573793bf237fef2aee056a6b9c
MikeGsus <mike08ahj@hotmail.com>
2024-07-31 13:40:16 -0600

    feat(Nosotros): optimize WeAre and RadiusToday components with useTransition hook

--- frontend/src/app/aboutus/WeAre.tsx
+++ frontend/src/app/aboutus/WeAre.tsx
@@ -1,44 +1,18 @@
 "use client"
-import { FC, useState, useEffect, useRef } from "react";
+import { FC, useRef } from "react";
+import useTransition from "@/hooks/useTransition";
 
-const baseTransitionClasses = "transition-all duration-[12000ms]";
-
-const customTransitionStyles = {
-  transition: 'opacity 2.5s, transform 2.5s'
-};
 
 const WeAre : FC = () => {
-  const [isVisible, setIsVisible] = useState(false);
-
   const bannerRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          setIsVisible(true);
-          observer.disconnect();
-        }
-      },
-      { threshold: 0.1 }
-    );
-
-    if (bannerRef.current) {
-      observer.observe(bannerRef.current);
-    }
-
-    return () => {
-      if (observer && bannerRef.current) {
-        observer.unobserve(bannerRef.current);
-      }
-    };
-  }, []);
+  
+  const {applicableYStyle, customTransitionStyles} = useTransition(bannerRef);
 
   return (
     <div className="w-full flex bg-primary text-white justify-center text-center" >
       <div ref={bannerRef} className="container flex flex-row justify-evenly">
         <div
-          className={`flex flex-col justify-center items-center mx-5 ${baseTransitionClasses} ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
+          className={`flex flex-col justify-center items-center mx-5 ${applicableYStyle}`}
           style={customTransitionStyles} >
           <h2 className="text-xl text-red-500">
             SOMOS
@@ -51,7 +25,7 @@ const WeAre : FC = () => {
           </p>
         </div>
         <div
-          className={`flex flex-col justify-center items-center mx-5 ${baseTransitionClasses} ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}
+          className={`flex flex-col justify-center items-center mx-5 ${applicableYStyle}`}
           style={customTransitionStyles} >
           <h2 className="text-xl text-red-500">
             SOMOS
