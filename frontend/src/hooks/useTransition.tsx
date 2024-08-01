03f5efda4ed58946211ee5260e669e580f1fed14
MikeGsus <mike08ahj@hotmail.com>
2024-08-01 13:34:04 -0600

    chore(Nosotros): update useTransition hook to include negative translations

--- frontend/src/hooks/useTransition.tsx
+++ frontend/src/hooks/useTransition.tsx
@@ -6,13 +6,16 @@ export const customTransitionStyles = {
   transition: 'opacity 2.5s, transform 2.5s'
 };
 
-export const finalYStyles = "opacity-100 translate-y-0"
-export const finalXStyles = "opacity-100 translate-x-0"
+export const showUp = "opacity-100"
+export const hide = "opacity-0"
 
-export const initialYStyles = "opacity-0 translate-y-10"
-export const initialXStyles = "opacity-0 translate-x-10"
-export const initialYNegativeStyles = "opacity-0 -translate-y-10"
-export const initialXNegativeStyles = "opacity-0 -translate-x-10"
+export const finalYStyles = `${showUp} translate-y-0`
+export const finalXStyles = `${showUp} translate-x-0`
+
+export const initialYStyles = `${hide} translate-y-10`
+export const initialXStyles = `${hide} translate-x-10`
+export const initialYNegativeStyles = `${hide} -translate-y-10`
+export const initialXNegativeStyles = `${hide} -translate-x-10`
 
 
 const useTransition = (ref: any) => {
@@ -45,12 +48,14 @@ const useTransition = (ref: any) => {
   const applicableXStyle = `${baseTransitionClasses} ${isVisible ? finalXStyles : initialXStyles}`
   const applicableYNegativeStyle = `${baseTransitionClasses} ${isVisible ? finalYStyles : initialYNegativeStyles}`
   const applicableXNegativeStyle = `${baseTransitionClasses} ${isVisible ? finalXStyles : initialXNegativeStyles}`
+  const appearingStyle = `${baseTransitionClasses} ${isVisible ? showUp : hide}`
 
   return {
     applicableYStyle,
     applicableYNegativeStyle,
     applicableXStyle,
     applicableXNegativeStyle,
+    appearingStyle,
     customTransitionStyles
   };
 }
