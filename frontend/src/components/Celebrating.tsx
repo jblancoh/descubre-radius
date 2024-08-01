a3b4e48ad0b9c2e9af28de9697de704b7a48da75
MikeGsus <mike08ahj@hotmail.com>
2024-08-01 13:34:24 -0600

    feat(Nosotros): optimize banner transitions with useTransition hook

--- frontend/src/components/Celebrating.tsx
+++ frontend/src/components/Celebrating.tsx
@@ -1,12 +1,25 @@
-
+"use client";
+import { useRef } from "react";
+import useTransition from "@/hooks/useTransition"
 
 
 const Celebrating = () => {
+    const bannerRef = useRef(null);
+    const {applicableYStyle, customTransitionStyles} = useTransition(bannerRef);
+
     return (
-        <div className="w-full flex justify-center items-center p-20 bg-red-500">
+        <div ref={bannerRef} className="w-full flex justify-center items-center p-20 bg-red-500">
             <div className="text-center text-white">
-                <h1 className="text-3xl font-bold mb-4">Celebrando 10 años</h1>
-                <p className="max-w-lg mx-auto">
+                <h1 
+                    className={`text-3xl font-bold mb-4 ${applicableYStyle}`}
+                    style={customTransitionStyles}
+                >
+                    Celebrando 10 años
+                </h1>
+                <p 
+                    className={`max-w-lg mx-auto ${applicableYStyle}`}
+                    style={customTransitionStyles}
+                >
                     Nuestra pasión sigue intacta, y seguimos ayudando a nuestros clientes con estrategias, herramientas y procesos para crear una inteligencia continua, minimizar el riesgo en la toma de decisiones e impulsar el valor en toda la empresa.
                 </p>
                 <div className="mt-10">
