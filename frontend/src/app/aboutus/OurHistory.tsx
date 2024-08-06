f287b907c2ed28872945df955bc565b2c79cf79f
MikeGsus <mike08ahj@hotmail.com>
2024-08-06 13:29:31 -0600

    feat(OurHistory): improve layout and add images to timeline

--- frontend/src/app/aboutus/OurHistory.tsx
+++ frontend/src/app/aboutus/OurHistory.tsx
@@ -40,20 +40,21 @@ const OurHistory: FC = () => {
   const bannerRef = useRef(null);
   const {applicableYStyle, customTransitionStyles} = useTransition(bannerRef);
   return (
-    <div className="w-full flex flex-col justify-center text-white bg-[#211039] py-20">
+    <div className="w-full flex flex-col justify-center text-white bg-[#211039] py-20 lg:px-20">
       <Label
         className={`text-2xl leading-8 font-bold w-100 text-center`}
       >
         Nuestra Historia
       </Label>
-      <div className="container p-10 lg:flex lg:border-t-2">
+      <img src="/img/linea-historia.svg" alt="Time Line" className='hidden lg:block mt-5' />
+      <div className="container flex flex-col items-center my-10 lg:flex-row lg:items-start lg:-mt-6 md:px-20 lg:-ml-10">
         {timeLine.map((item, index) => {
           const { year, title, content } = item
           return (
-          <div key={index} className="flex text-justify p-4">
-            <span className="w-4 h-4 rounded-full mx-5 bg-white" />
+          <div key={index} className="flex flex-1 w-full md:w-2/3 lg:flex-col lg:justify-start lg:items-center text-left py-4">
+            <span className="w-4 h-4 rounded-full mx-5 px-2 bg-white" />
             <p className="text-2xl font-black mr-5">{year}</p>
-            <div className="text-white/[.7] text-sm mx-5">
+            <div className="text-white/[.7] text-sm ">
               <p className="-ml-4">{title}</p>
               <ul className='list-disc'>
                 {content.map((itemContent, index) => (
@@ -64,6 +65,7 @@ const OurHistory: FC = () => {
           </div>
         )})}
       </div>
+      <img src="/img/footer-nuestra-historia.svg" alt="Time Line" className='hidden w-4/6 ml-80 lg:block' />
     </div>
   );
 };
