099fe32068a7ce0906269bac4f5299e4660d3e37
jblancoh <zlatanjon@hotmail.com>
2024-08-05 19:14:23 -0600

    ✨ (banner frameworks): Add BannerFrameworks component to analytics and tech pages

--- frontend/src/app/tech/page.tsx
+++ frontend/src/app/tech/page.tsx
@@ -2,6 +2,7 @@ import BannerSolutions from "@/components/BannerSolutions";
 import Contact from "@/components/Contact";
 import SuccesfulCases from "@/components/SuccesfulCases";
 import WhyRadiusSimple from "@/components/WhyRadiusSimple";
+import BannerFrameworks from "@/components/BannerFrameworks";
 
 
 const Page = () => {
@@ -10,6 +11,7 @@ const Page = () => {
       <BannerSolutions type="tech"/>
       <SuccesfulCases />
       <WhyRadiusSimple type="tech" />
+      <BannerFrameworks type="tech" />
       <Contact />
     </div>
   )
