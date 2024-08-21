e832e3567d5bb5fdf2956b430bceb3ff6ececbe8
MikeGsus <mike08ahj@hotmail.com>
2024-08-21 00:01:43 -0600

    feat(Tech): Add Roles component and update page layout

--- frontend/src/app/tech/page.tsx
+++ frontend/src/app/tech/page.tsx
@@ -4,16 +4,17 @@ import SuccesfulCases from "@/components/SuccesfulCases";
 import WhyRadiusSimple from "@/components/WhyRadiusSimple";
 import BannerFrameworks from "@/components/BannerFrameworks";
 import BannerService from "@/components/BannerService";
-
+import Roles from './Roles';
 
 const Page = () => {
   return (
     <div>
       <BannerService type="tech"/>
       <BannerSolutions type="tech"/>
+      <BannerFrameworks type="tech" />
+      <Roles />
       <SuccesfulCases />
       <WhyRadiusSimple type="tech" />
-      <BannerFrameworks type="tech" />
       <Contact />
     </div>
   )
