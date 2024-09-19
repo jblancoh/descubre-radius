b4b7d1d5bf3d780cbd302d66171dd6e0f178476a
jblancoh <zlatanjon@hotmail.com>
2024-09-19 16:29:53 -0600

    Update AccordionTrigger component to remove unnecessary type annotation

--- frontend/src/components/ui/accordion.tsx
+++ frontend/src/components/ui/accordion.tsx
@@ -22,7 +22,7 @@ AccordionItem.displayName = "AccordionItem"
 
 const AccordionTrigger = React.forwardRef<
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Header className="flex">
     <AccordionPrimitive.Trigger
