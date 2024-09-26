6e28ee3439c4fbe4b0ddf960615c53d08b385c41
jblancoh <zlatanjon@hotmail.com>
2024-09-25 20:19:19 -0600

    :ambulance: (ecosystem.config): Fix environment to cms

--- cms-radius/ecosystem.config.js
+++ cms-radius/ecosystem.config.js
@@ -1,4 +1,5 @@
-module.exports = ({ env }) => ({
+require('dotenv').config();
+module.exports = {
     apps: [
       {
       name: 'Descubre radius',
@@ -6,21 +7,8 @@ module.exports = ({ env }) => ({
       script: 'npm',
       args: 'start',
       env: {
-        APP_KEYS: env.array('APP_KEYS'),
-        API_TOKEN_SALT: env.array('API_TOKEN_SALT'),
-        ADMIN_JWT_SECRET: env.array('ADMIN_JWT_SECRET'),
-        JWT_SECRET: env.array('JWT_SECRET'),
-        NODE_ENV: env.array('NODE_ENV'),
-        DATABASE_HOST: env.array('DATABASE_HOST'),
-        DATABASE_PORT: env.array('DATABASE_PORT'),
-        DATABASE_NAME: env.array('DATABASE_NAME'),
-        DATABASE_USERNAME: env.array('DATABASE_USERNAME'),
-        DATABASE_PASSWORD: env.array('DATABASE_PASSWORD'),
-        AWS_ACCESS_KEY_ID: env.array('AWS_ACCESS_KEY_ID'),
-        AWS_ACCESS_SECRET: env.array('AWS_ACCESS_SECRET'),
-        AWS_REGION: env.array('AWS_REGION'),
-        AWS_BUCKET_NAME: env.array('AWS_BUCKET_NAME'),
+        ...process.env,
       },
     }
   ],
-})
+}
